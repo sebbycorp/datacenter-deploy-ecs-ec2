800589f026099164567f4c8e393baaac03c74c7a
sebbycorp <sebastian.maniak@hashicorp.com>
2022-12-22 12:41:54 -0500

    updates

--- tls-hashicups/outputs.tf
+++ tls-hashicups/outputs.tf
@@ -1,5 +1,5 @@
 output "client_lb_address" {
-  value = "http://${aws_lb.example_client_app.dns_name}:9090/ui"
+  value = "http://${aws_lb.example_client_app.dns_name}"
 }
 
 output "Consul_ui_address" {
